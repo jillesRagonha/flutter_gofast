ae5a138a1f094cb863fdd775170db4f114c18ce3
jillesRagonha <jilles.ragonha@hotmail.com>
2020-05-12 00:16:16 -0300

    continuando aula

--- lib/app/modules/intro/intro_page.dart
+++ lib/app/modules/intro/intro_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gofast/app/core/internacionalizacao/app_translate.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'intro_controller.dart';
 
 class IntroPage extends StatefulWidget {
   final String title;
+
   const IntroPage({Key key, this.title = "Intro"}) : super(key: key);
 
   @override
@@ -11,17 +13,40 @@ class IntroPage extends StatefulWidget {
 }
 
 class _IntroPageState extends ModularState<IntroPage, IntroController> {
+  int currentIndex = 0;
+
   //use 'controller' variable to access controller
 
   @override
   Widget build(BuildContext context) {
+    var _height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        AppTranslate(context).text('intro.welcome'),
+                        style: Theme.of(context).textTheme.headline5.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                      SlideWidget()
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
