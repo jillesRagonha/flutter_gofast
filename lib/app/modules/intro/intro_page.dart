c63c5ab47f5a519ec1b557a202d30e94489f798a
jillesRagonha <jilles.ragonha@hotmail.com>
2020-05-12 10:08:55 -0300

    concluido aula 10

--- lib/app/modules/intro/intro_page.dart
+++ lib/app/modules/intro/intro_page.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_gofast/app/core/internacionalizacao/app_translate.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+import '../../core/consts/colors_const.dart';
+import '../../core/internacionalizacao/app_translate.dart';
 import 'intro_controller.dart';
+import 'widgets/slide_dots_widget.dart';
+import 'widgets/slide_widget.dart';
 
 class IntroPage extends StatefulWidget {
   final String title;
@@ -15,38 +18,86 @@ class IntroPage extends StatefulWidget {
 class _IntroPageState extends ModularState<IntroPage, IntroController> {
   int currentIndex = 0;
 
-  //use 'controller' variable to access controller
-
   @override
   Widget build(BuildContext context) {
     var _height = MediaQuery.of(context).size.height;
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        AppTranslate(context).text('intro.welcome'),
-                        style: Theme.of(context).textTheme.headline5.copyWith(
-                              fontWeight: FontWeight.bold,
-                            ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      AppTranslate(context).text('intro.welcome'),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    SlideWidget(
+                      onPageChanged: (index) {
+                        setState(() {
+                          currentIndex = index;
+                        });
+                      },
+                    ),
+                    SlideDotsWidget(
+                      currentPage: currentIndex,
+                    ),
+                    SizedBox(
+                      height: _height * 0.06,
+                    ),
+                    RaisedButton(
+                        onPressed: () {},
+                        child: Text(
+                          AppTranslate(context).text('intro.login_now'),
+                          style: Theme.of(context).textTheme.bodyText2.copyWith(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        )),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Container(
+                            height: 1, color: ColorsConst.grey400, width: 100),
+                        Text(
+                          AppTranslate(context).text('intro.or'),
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText2
+                              .copyWith(color: ColorsConst.grey600),
+                        ),
+                        Container(
+                            height: 1, color: ColorsConst.grey400, width: 100),
+                      ],
+                    ),
+                    SizedBox(
+                      height: _height * 0.04,
+                    ),
+                    RaisedButton(
+                      color: Colors.red,
+                      onPressed: () {},
+                      child: Text(
+                        AppTranslate(context).text('intro.login_google'),
+                        style: Theme.of(context).textTheme.bodyText2.copyWith(
+                            color: Colors.white, fontWeight: FontWeight.bold),
                       ),
-                      SlideWidget()
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
